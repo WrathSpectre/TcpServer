31b603226a15365d235e38ac587f3d9a081811b2
Sebastian Słomka <30845528+WrathSpectre@users.noreply.github.com>
2018-02-06 23:35:49 +0100

    Update TcpServer.java

--- TcpServer.java
+++ TcpServer.java
@@ -3,14 +3,43 @@ import java.net.Socket;
 import java.net.ServerSocket;
 
 public class TcpServer extends Thread {
-    public static final int port = 8080;
-    private boolean connection_status = false;
+    private ServerSocket serverSocket;
+    private Socket socket;
 
-    ServerSocket serverSocket;
-    Socket socket;
+    private int port;
+    public boolean connection_status = false;
 
-    BufferedReader inReader;
-    PrintWriter outWriter;
+    private BufferedReader inReader;
+    private PrintWriter outWriter;
+
+    private String message;
+
+    public TcpServer(final int port) {
+        this.port = port;
+    }
+
+    public TcpServer() {
+        if ((port = getAvailablePort()) == -1)
+            System.out.println("No available ports!!!!!!!!");
+        else
+            this.start();
+    }
+
+    private int getAvailablePort() {
+        ServerSocket server;
+
+        for (int port = 1000; port < 1024; port++) {
+            try {
+                server = new ServerSocket(port);
+                server.close();
+                return port;
+            } catch (IOException ex) {
+                System.err.println("no available ports");
+            }
+        }
+
+        return -1;
+    }
 
     @Override
     public void run() {
@@ -29,10 +58,8 @@ public class TcpServer extends Thread {
                 inReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 outWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 
-                while (connection_status) {
-                   String message = inReader.readLine();
-                   System.out.println(message);
-                }
+                while (connection_status) 
+                    message = inReader.readLine();
 
             } catch (Exception e) {
                 System.out.println("Server: Error");
@@ -48,7 +75,13 @@ public class TcpServer extends Thread {
         }
     }
 
-    public void send(final String data) {
+    public String getMessage() {
+        if(message != null) 
+            return message;
+        return null;
+    }
+
+    public void setMessage(final String data) {
         if (outWriter != null && !outWriter.checkError()) {
             outWriter.println(data);
             outWriter.flush();
